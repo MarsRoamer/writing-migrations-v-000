d785dcb98b743a3cf7c678098310ca01abb7bf03
Jonathan Tiede <skysoldiermars@gmail.com>
2018-04-09 21:42:52 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,5 +1,14 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
 
+  # sql = <<-SQL
+  #   CREATE TABLE IF NOT EXISTS students (
+  #     name TEXT
+  #   )
+  #   SQL
+  # 
+  #   ActiveRecord::Base.connection.execute(sql)
+  sql = 'DROP TABLE IF NOT EXISTS students'
+  ActiveRecord::Base.conection.execute(sql)
 
     def change
       create_table :students do |t|
